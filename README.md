49ecbd5cc1346e10f7b4b38b868db9fafcfb0a77
lastemp <emmanuelochieng797@gmail.com>
2024-01-25 12:07:11 +0300

    1st commit

--- README.md
+++ README.md
@@ -1,1 +1,28 @@
 # cs50_ai_shopping
+
+This is an AI program that predicta whether online shopping customers will complete a purchase.
+It forms part of the projects for the course [CS50’s Introduction to Artificial Intelligence with Python](https://cs50.harvard.edu/ai/2024/).
+
+The program has below listed dependency:
+- [Scikit-learn](https://scikit-learn.org/stable/) Scikit-learn is an open source machine learning library that supports supervised and unsupervised learning.
+
+## Usage
+
+All the following commands assume that your current working directory is _this_ directory. I.e.:
+
+```console
+$ pwd
+.../cs50_ai_shopping
+```
+
+1. Install the required Python package (Scikit-learn) for the project:
+
+   ```sh
+   pip3 install scikit-learn
+   ```
+   
+1. Run the application:
+
+   ```sh
+   py shopping.py shopping.csv
+   ```
---
+++ shopping.py
@@ -0,0 +1,167 @@
+import csv
+import sys
+
+from sklearn.model_selection import train_test_split
+from sklearn.neighbors import KNeighborsClassifier
+
+TEST_SIZE = 0.4
+
+
+def main():
+
+    # Check command-line arguments
+    if len(sys.argv) != 2:
+        sys.exit("Usage: python shopping.py data")
+
+    # Load data from spreadsheet and split into train and test sets
+    evidence, labels = load_data(sys.argv[1])
+    X_train, X_test, y_train, y_test = train_test_split(
+        evidence, labels, test_size=TEST_SIZE
+    )
+
+    # Train model and make predictions
+    model = train_model(X_train, y_train)
+    predictions = model.predict(X_test)
+    sensitivity, specificity = evaluate(y_test, predictions)
+
+    # Print results
+    print(f"Correct: {(y_test == predictions).sum()}")
+    print(f"Incorrect: {(y_test != predictions).sum()}")
+    print(f"True Positive Rate: {100 * sensitivity:.2f}%")
+    print(f"True Negative Rate: {100 * specificity:.2f}%")
+
+
+def load_data(filename):
+    """
+    Load shopping data from a CSV file `filename` and convert into a list of
+    evidence lists and a list of labels. Return a tuple (evidence, labels).
+
+    evidence should be a list of lists, where each list contains the
+    following values, in order:
+        - Administrative, an integer
+        - Administrative_Duration, a floating point number
+        - Informational, an integer
+        - Informational_Duration, a floating point number
+        - ProductRelated, an integer
+        - ProductRelated_Duration, a floating point number
+        - BounceRates, a floating point number
+        - ExitRates, a floating point number
+        - PageValues, a floating point number
+        - SpecialDay, a floating point number
+        - Month, an index from 0 (January) to 11 (December)
+        - OperatingSystems, an integer
+        - Browser, an integer
+        - Region, an integer
+        - TrafficType, an integer
+        - VisitorType, an integer 0 (not returning) or 1 (returning)
+        - Weekend, an integer 0 (if false) or 1 (if true)
+
+    labels should be the corresponding list of labels, where each label
+    is 1 if Revenue is true, and 0 otherwise.
+    """
+    evidence = []
+    labels = []
+
+    with open(filename, 'r') as csvfile:
+        reader = csv.reader(csvfile)
+        next(reader)  # Skip header row
+
+        for row in reader:
+            processed_row = [
+                int(row[0]),  # Administrative
+                float(row[1]),  # Administrative_Duration
+                int(row[2]),  # Informational
+                float(row[3]),  # Informational_Duration
+                int(row[4]),  # ProductRelated
+                float(row[5]),  # ProductRelated_Duration
+                float(row[6]),  # BounceRates
+                float(row[7]),  # ExitRates
+                float(row[8]),  # PageValues
+                float(row[9]),  # SpecialDay
+                int(get_month(row[10])),  # Month
+                int(row[11]),  # OperatingSystems
+                int(row[12]),  # Browser
+                int(row[13]),  # Region
+                int(row[14]),  # TrafficType
+                int(row[15] == 'Returning_Visitor'),  # VisitorType
+                int(row[16] == 'TRUE'),  # Weekend
+            ]
+            evidence.append(processed_row)
+            labels.append(int(row[17] == 'TRUE'))  # Revenue
+
+    return evidence, labels
+
+
+def train_model(evidence, labels):
+    """
+    Given a list of evidence lists and a list of labels, return a
+    fitted k-nearest neighbor model (k=1) trained on the data.
+    """
+    model = KNeighborsClassifier(n_neighbors=1)
+    model.fit(evidence, labels)
+    return model
+
+
+def evaluate(labels, predictions):
+    """
+    Given a list of actual labels and a list of predicted labels,
+    return a tuple (sensitivity, specificity).
+
+    Assume each label is either a 1 (positive) or 0 (negative).
+
+    `sensitivity` should be a floating-point value from 0 to 1
+    representing the "true positive rate": the proportion of
+    actual positive labels that were accurately identified.
+
+    `specificity` should be a floating-point value from 0 to 1
+    representing the "true negative rate": the proportion of
+    actual negative labels that were accurately identified.
+    """
+    true_positives = sum(label == 1 and prediction ==
+                         1 for label, prediction in zip(labels, predictions))
+    false_negatives = sum(label == 1 and prediction ==
+                          0 for label, prediction in zip(labels, predictions))
+    true_negatives = sum(label == 0 and prediction ==
+                         0 for label, prediction in zip(labels, predictions))
+    false_positives = sum(label == 0 and prediction ==
+                          1 for label, prediction in zip(labels, predictions))
+
+    sensitivity = true_positives / (true_positives + false_negatives)
+    specificity = true_negatives / (true_negatives + false_positives)
+
+    return sensitivity, specificity
+
+
+def get_month(month):
+    match month.lower():
+        case 'jan':
+            return 0
+        case 'feb':
+            return 1
+        case 'mar':
+            return 2
+        case 'apr':
+            return 3
+        case 'may':
+            return 4
+        case 'june':
+            return 5
+        case 'jul':
+            return 6
+        case 'aug':
+            return 7
+        case 'sep':
+            return 8
+        case 'oct':
+            return 9
+        case 'nov':
+            return 10
+        case 'dec':
+            return 11
+        case _:
+            print(month.lower())
+            raise IndexError
+
+
+if __name__ == "__main__":
+    main()
